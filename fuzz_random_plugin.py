b2a607cc05a0d76db95fb967e2419608f1afb78c
Charles-Isaac Côté <charl_i_c@hotmail.com>
2023-04-20 12:44:05 -0400

    patch the installation and start fuzzing a random plugin
    And the Lord spake, saying, "Verily, verily, I say unto thee: when thou encountereth a piece of software, and knoweth not its vulnerabilities, thou shalt take a fuzzer and use it to probe the depths of its code. Yea, thou shalt bombard it with input of all kinds, and lo, thou shalt see if it doth break. For as a grenade doth explode when its pin is pulled, so too shall a fuzzer reveal the weaknesses of thy software."
    
    And the people did marvel at this strange new tool, and they did question the Lord, saying "But Lord, how shall we know if our software be secure?" And the Lord did reply, "Use the fuzzer, my children, and thou shalt see if thy code be solid or full of holes. And if thy code be full of holes, then fix it, lest the hackers come and exploit thy vulnerabilities."
    
    And so the people did take the fuzzer, and they did bombard their software with all manner of input, and they did see the bugs and vulnerabilities that lay within. And they did fix their code, and they did rejoice, for they knew that their software was now secure.
    
    But woe unto those who did not use the fuzzer, for they did suffer the wrath of the hackers, and their code was riddled with exploits and vulnerabilities. And the Lord did look upon them with disdain, saying, "Verily, verily, I say unto thee, ye should have used the fuzzer."

--- fuzz_random_plugin.py
+++ fuzz_random_plugin.py
@@ -2,13 +2,43 @@ import requests
 from subprocess import call
 import os
 import random
+import fuzz_plugin
+import shutil
+import os
+
+# quickly patch the installation
+
+fuzzer_container_file = 'fuzzer_container.py'
+docker_compose_file = 'docker-compose.yml'
+
+with open(fuzzer_container_file, 'r') as file:
+    lines = file.readlines()
+
+current_folder = os.path.basename(os.getcwd())
+if lines[152] != f'    network_name = "{current_folder}_network2"':
+    lines[152] = f'    network_name = "{current_folder}_network2"'
+
+    print(f"changed line 152 of {fuzzer_container_file}")
+    with open(fuzzer_container_file, 'w') as file:
+        file.writelines(lines)
+
+with open(docker_compose_file, 'r') as file:
+    lines = file.readlines()
+
+if lines[18].strip() == 'container_name: wordpress1':
+    del lines[18]
+
+    print(f"deleted line 19 of {docker_compose_file}")
+    with open(docker_compose_file, 'w') as file:
+        file.writelines(lines)
+
 
 # Set the API endpoint and parameters
 endpoint = "https://api.wordpress.org/plugins/info/1.2/"
 params = {
     "action": "query_plugins",
     "request[per_page]": 250,  # number of plugins per page
-    "request[page]": 1,  # page number
+    "request[page]": random.randint(0, 222),  # page number # there was 222+1 pages last time I checked
 }
 
 # Make the API request and retrieve the response
@@ -18,4 +48,4 @@ data = response.json()
 # Start fuzzing a random plugin
 max_index = len(data["plugins"])
 plugin = data["plugins"][random.randint(0, max_index-1)]
-os.system(f"fuzz_plugin.py {plugin['slug']} --version {plugin['version']}")
+fuzz_plugin.fuzz_plugin(plugin['slug'])
