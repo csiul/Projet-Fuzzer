841b583bea25608cfa515aaa4bbfe57e7e0eadd4
Paradise <SAPAR85@ulaval.ca>
2023-11-19 23:09:53 -0500

    Ajout du routeur pour des endpoints liés à WordPress

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ API permettant d'utiliser WPGarlic.
 
 from fastapi import FastAPI
 
-from routers import api_status, fuzz_plugin
+from routers import api_status, fuzz_plugin, wordpress
 
 # Create FastAPI
 app = FastAPI(debug=True, title="Projet Fuzzer - WPGarlic API", docs_url=None)
@@ -12,3 +12,4 @@ app = FastAPI(debug=True, title="Projet Fuzzer - WPGarlic API", docs_url=None)
 # Register routers
 app.include_router(api_status.router)
 app.include_router(fuzz_plugin.router)
+app.include_router(wordpress.router)
---
+++ api/routers/wordpress.py
@@ -0,0 +1,70 @@
+"""
+Router : WordPress
+"""
+from math import ceil
+from random import randint
+
+import requests
+from fastapi import APIRouter
+
+BASE_URL = "https://api.wordpress.org/plugins/info/1.2/"
+
+router = APIRouter(prefix="/wordpress")
+
+
+@router.get('/plugins')
+def get_plugins():
+    """
+    Obtient la liste des plugins WordPress.
+    Agit comme proxy vers le site officiel wordpress.org.
+    ATTENTION, cette requête n'est présentement pas optimisée, 1.8 minutes pour obtenir la liste complète.
+    """
+    page = 1
+    plugins = []
+    while True:
+        params = {
+            "action": "query_plugins",
+            "request[per_page]": 250,  # number of plugins per page
+            "request[page]": page
+        }
+        response = requests.get(BASE_URL, params=params, timeout=30)
+        data = response.json()
+        plugins += data['plugins']
+        if page < data['info']['pages']:
+            page += 1
+        else:
+            break
+    return plugins
+
+
+@router.get('/plugins_count')
+def get_plugins_count():
+    """
+    Obtient le nombre total de plugins.
+    """
+    params = {"action": "query_plugins"}
+    response = requests.get(BASE_URL, params=params, timeout=30)
+    data = response.json()
+    info = data['info']['results']
+    return {
+        'count': info
+    }
+
+
+@router.get('/random_plugin')
+def get_random_plugin():
+    """
+    Obtient un plugin aléatoire parmis les ~5K plugins de WordPress.org.
+    """
+    total = get_plugins_count()['count']
+    i = randint(1, total)
+    page = ceil(i / 250.0)
+    params = {
+        "action": "query_plugins",
+        "request[per_page]": 250,  # number of plugins per page
+        "request[page]": page
+    }
+    response = requests.get(BASE_URL, params=params, timeout=30)
+    data = response.json()
+    plugin = data['plugins'][i - 250 * (page - 1) - 1]
+    return plugin
