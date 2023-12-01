348f980db90285565e9a5e4c5e086659fcbfecf3
Olivier <128328913+OlivierForguesNhan@users.noreply.github.com>
2023-12-01 17:45:42 -0500

    First dummy SQL query on home page for testing

--- app/app/blueprints/index_page/routes.py
+++ app/app/blueprints/index_page/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, make_response, render_template, current_app, Response
+from sqlalchemy import text
+from app.ext.sqlalchemy.database import db_session
 
 
 blueprint: Blueprint = Blueprint(
@@ -11,9 +13,13 @@ blueprint: Blueprint = Blueprint(
 
 @blueprint.route("/", methods=["get"])
 def index_route() -> Response:
+
+    result = db_session.execute(text("SELECT 1"))
+    value = result.scalar()
     return make_response(
         render_template(
             "index.jinja2",
+            sqlvalue=value
         )
     )
 
