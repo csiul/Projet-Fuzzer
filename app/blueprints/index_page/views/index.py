f3899de3d07e40528199420cbfd1072d73a07150
Sophie Goulet <sophie.goulet.7@ulaval.ca>
2023-12-22 08:49:39 -0500

    update code for pylint

--- app/blueprints/index_page/views/index.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_plugins():
-    r = requests.get('https://api.wordpress.org/plugins/info/1.1/?action=query_plugins&request[page]=1&request[per_page]=250&request[browse]=popular')
-
-    results = [plugin for plugin in r.json()['plugins']]
-
-    return results
---
+++ web/app/blueprints/index_page/views/index.py
@@ -0,0 +1,15 @@
+# Module for getting the list of plugins from the WordPress API
+import requests
+
+
+# Get the list of plugins from the WordPress API
+def get_plugins():
+    base_url = 'https://api.wordpress.org/plugins/info/1.1/'
+    r = requests.get(
+        base_url +
+        '?action=query_plugins&request[page]=1&request[per_page]=250&request[browse]=popular',
+        timeout=30)
+
+    results = [plugin for plugin in r.json()['plugins']]
+
+    return results
